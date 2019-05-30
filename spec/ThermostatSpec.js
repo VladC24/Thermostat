ba2cdc587aea91c8f73f1aa96a1638c3af07eb96
VladC <vlad.chiriac24@gmail.com>
2019-05-30 18:06:57 +0100

    Add current usage and maximum temp + refactor

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -28,25 +28,67 @@ describe ("Thermostat", function() {
 		expect(thermostat.temperature).toEqual(10);
 	});
 
-	it('starts with power saving mode on', function() {
-		expect(thermostat.powerSavingMode).toBe(true);
-	});
-
-	it('has a maximum temperature of 25 with power mode on', function() {
-		for(var i = 0; i < 6; i++) {
+	it ('allows for the maximum temperature to be 32 degrees', function() {
+		thermostat.powerSavingMode = false;
+		for(var i=0; i<13; i++) {
 			thermostat.up()
 		}
-		expect(thermostat.temperature).toEqual(25);
+		expect(thermostat.temperature).toEqual(32);
 	});
 
-	it('can switch power mode off', function() {
-		thermostat.powerSavingModeOff();
-		expect(thermostat.powerSavingMode).toBe(false);
+	describe ('Switching between power modes', function() {
+		
+		it('starts with power saving mode on', function() {
+			expect(thermostat.powerSavingMode).toBe(true);
+		});
+
+		it('can switch power mode off', function() {
+			thermostat.powerSavingModeOff();
+			expect(thermostat.powerSavingMode).toBe(false);
+		});
+
+		it('can switch power mode on', function() {
+			thermostat.powerSavingModeOff();
+			thermostat.powerSavingModeOn();
+			expect(thermostat.powerSavingMode).toBe(true);
+		});
+
+		it('has a maximum temperature of 25 with power mode on', function() {
+			for(var i = 0; i < 6; i++) {
+				thermostat.up()
+			}
+			expect(thermostat.temperature).toEqual(25);
+		});
+
 	});
 
 	it('can reset the temperature back to 20', function() {
 		thermostat.reset();
 		expect(thermostat.temperature).toEqual(20);
+	});
+
+	describe ('Usage levels', function() {
+
+		it('shows current energy usage is high above 25', function() {
+			thermostat.powerSavingMode = false;
+			for(var i = 0; i < 6; i++) {
+				thermostat.up()
+			}
+			expect(thermostat.currentUsage()).toEqual('High Usage');
+		});
+
+		it('shows current energy usage is medium between 18 and 25', function() {
+			expect(thermostat.currentUsage()).toEqual('Medium Usage');
+		});
+
+		it('shows current energy usage is low under 18', function() {
+			thermostat.powerSavingMode = true;
+			for(var i = 0; i < 3; i++) {
+				thermostat.down()
+			}
+			expect(thermostat.currentUsage()).toEqual('Low Usage');
+		});
+
 	});
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,17 @@
 function Thermostat() {
 	this.MINIMUM_TEMPERATURE = 10;
+	this.MAXIMUM_TEMPERATURE = 32;
+	this.HIGH_USAGE_LIMIT = 25;
+	this.LOW_USAGE_LIMIT = 18;
 	this.temperature = 20;
-	this.powerSavingMode = true
+	this.powerSavingMode = true;
 };
 
 Thermostat.prototype.up = function() {
 	if(this.powerSavingMode === true && this.temperature === 25) {
+		return;
+	}
+	if(this.powerSavingMode === false && this.temperature === 32) {
 		return;
 	}
 	this.temperature += 1;
@@ -22,6 +28,20 @@ Thermostat.prototype.powerSavingModeOff = function() {
 	this.powerSavingMode = false
 };
 
+Thermostat.prototype.powerSavingModeOn = function() {
+	this.powerSavingMode = true
+};
+
 Thermostat.prototype.reset = function() {
 	this.temperature = 20;
+};
+
+Thermostat.prototype.currentUsage = function() {
+		if (this.temperature >= this.HIGH_USAGE_LIMIT) {
+		return 'High Usage';
+		} else if (this.temperature < this.HIGH_USAGE_LIMIT && this.temperature > this.LOW_USAGE_LIMIT) {
+			return 'Medium Usage';
+		} else if (this.temperature <= this.LOW_USAGE_LIMIT) {
+			return 'Low Usage';
+		}
 };
